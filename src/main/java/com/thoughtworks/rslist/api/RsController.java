ca8dd9e8954ef3352dad693987e986ad8c390efa
steven-x-dev <steven-dev@qq.com>
2020-09-16 02:29:06 +0800

    [steven] feat: implemented find one and find a range of events

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,37 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/rs")
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+
+  private List<RsEvent> rsList = new ArrayList<RsEvent>() {{
+    add(new RsEvent("第一条事件", "政治"));
+    add(new RsEvent("第二条事件", "经济"));
+    add(new RsEvent("第三条事件", "文化"));
+  }};
+
+  @GetMapping(path = "/find")
+  public RsEvent find(@RequestParam int index) {
+    return rsList.get(index - 1);
+  }
+
+  @GetMapping(path = "/list")
+  public List<RsEvent> list(@RequestParam(required = false) Integer start,
+                            @RequestParam(required = false) Integer end) {
+
+    if (start == null || end == null) {
+      return new ArrayList<>(rsList);
+    } else {
+      return new ArrayList<>(rsList.subList(start - 1, end));
+    }
+  }
+
 }
